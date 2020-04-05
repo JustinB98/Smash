3be9198dc33bdd75326d9472b7299a24e7ac6f02
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-05 15:16:56 -0400

    Added a demo main to keep prompting for input, then showing the parsed results

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 
 #include "input.h"
+#include "task.h"
 
 int main(int argc, const char *argv[], const char *env[]) {
 	size_t n = getpagesize();
@@ -13,7 +14,21 @@ int main(int argc, const char *argv[], const char *env[]) {
 		fflush(stdout);
 		result = get_input(stdin, &buf, &n, NULL);
 		if (result < 0) break;
-		printf("Input: %s", buf);
+		TASK *task = parse_task(buf);
+		if (task == TASK_EMPTY) continue;
+		else if (task == TASK_FAILED) break;
+		printf("Successful: %p\n", task);
+		printf("Input: %s\n", buf);
+		WORD_LIST *list = task->word_list;
+		while (list) {
+			printf("%s ", list->word);
+			list = list->next;
+		}
+		printf("\n");
+		printf("stdin: %s\n", task->stdin_path);
+		printf("stdout: %s\n", task->stdout_path);
+		printf("stderr: %s\n", task->stderr_path);
+		free_task(task);
 	}
 	free(buf);
 	if (result < 0) puts("");
