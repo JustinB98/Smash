ed290baa80ee95d38f046de1f36157bbe2c778b6
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-04 23:55:01 -0400

    Cleaned up the task parsing function

--- src/task.c
+++ src/task.c
@@ -21,28 +21,34 @@ void free_task(TASK *task) {
 	free(task);
 }
 
+static int allocate_task_variables(TASK **task, char **string_to_parse, WORD_LIST **word_list) {
+	*task = malloc(sizeof(TASK));
+	if (*task == NULL) goto free_task;
+	*string_to_parse = strdup(*string_to_parse);
+	if (string_to_parse == NULL) goto free_string_to_parse;
+	*word_list = malloc(sizeof(WORD_LIST));
+	if (word_list == NULL) goto free_word_list;
+	return 0;
+free_word_list:
+	free(*word_list);
+free_string_to_parse:
+	free(*string_to_parse);
+free_task:
+	free(*task);
+	return -1;
+}
+
 TASK *parse_task(char *string_to_parse) {
-	TASK *task = malloc(sizeof(TASK));
-	if (task == NULL) return TASK_FAILED;
-	string_to_parse = strdup(string_to_parse);
-	if (string_to_parse == NULL) {
-		free(task);
-		return TASK_FAILED;
-	}
-	if (string_to_parse == NULL) return TASK_FAILED;
+	TASK *task;
+	WORD_LIST *word_list;
+	if (allocate_task_variables(&task, &string_to_parse, &word_list)) return TASK_FAILED;
 	task->full_command = string_to_parse;
 	char *s = string_to_parse;
-	char *token = NULL;
-	WORD_LIST *word_list = malloc(sizeof(WORD_LIST));
-	if (word_list == NULL) {
-		free(task);
-		free(string_to_parse);
-		return TASK_FAILED;
-	}
 	word_list->next = NULL;
 	WORD_LIST *prev = NULL;
 	task->word_list = word_list;
 	size_t n_words = 0;
+	char *token = NULL;
 	while ((token = strtok_r(s, " ", &s))) {
 		if (*token == '\n') continue;
 		word_list->word = token;
