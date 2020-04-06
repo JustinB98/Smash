42deb041946184f8c38a52e67b13a34a53a43557
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-06 02:37:57 -0400

    Added a job module that will run jobs Right now it can only run a job on a child process with file redirection The parent just waits to reap the child. There is no support for background jobs yet

---
+++ include/job.h
@@ -0,0 +1,21 @@
+#ifndef JOB_H
+#define JOB_H
+
+#include "task.h"
+
+typedef enum job_status {
+	RUNNING,
+	STOPPED,
+	DONE
+} JOB_STATUS;
+
+typedef struct job {
+	TASK *task;
+	JOB_STATUS status;
+	pid_t pid;
+} JOB;
+
+void start_task(TASK *task, char *envp[]);
+
+
+#endif /* JOB_H */
---
+++ src/job.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+#include "task.h"
+#include "job.h"
+
+static void safe_open_file_for_reading(char *path, int *fd, int default_value) {
+	if (path == NULL) {
+		*fd = default_value;
+		return;
+	}
+	int possible_fd = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	if (possible_fd < 0) {
+		fprintf(stderr, "Could not open %s: %s\n", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	*fd = possible_fd;
+}
+
+static void open_stdin_path(char *stdin_path, int *stdin_fd) {
+	if (stdin_path == NULL) {
+		*stdin_fd = STDIN_FILENO;
+		return;
+	}
+	int fd = open(stdin_path, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Could not open %s: %s\n", stdin_path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	*stdin_fd = fd;
+}
+
+static void fill_redirection_fds(TASK *task, int *stdin_fd, int *stdout_fd, int *stderr_fd) {
+	safe_open_file_for_reading(task->stdout_path, stdout_fd, STDOUT_FILENO);
+	safe_open_file_for_reading(task->stderr_path, stderr_fd, STDERR_FILENO);
+	open_stdin_path(task->stdin_path, stdin_fd);
+}
+
+static void dup_fd(int fd, int default_value, char *type) {
+	if (fd != default_value) {
+		int result = dup2(fd, default_value);
+		if (result < 0) {
+			fprintf(stderr, "Could not dup2 %s: %s\n", type, strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+static void dup_fds(int stdin_fd, int stdout_fd, int stderr_fd) {
+	dup_fd(stdin_fd, STDIN_FILENO, "stdin");
+	dup_fd(stdout_fd, STDOUT_FILENO, "stdout");
+	dup_fd(stderr_fd, STDERR_FILENO, "stderr");
+}
+
+static void fill_argv(WORD_LIST *list, char **argv) {
+	WORD_LIST *current = list;
+	while (current) {
+		*argv = current->word;
+		++argv;
+		current = current->next;
+	}
+	*argv = NULL;
+}
+
+static void child_process_start_job(TASK *task, char *envp[]) {
+	int stdin_fd, stdout_fd, stderr_fd;
+	fill_redirection_fds(task, &stdin_fd, &stdout_fd, &stderr_fd);
+	dup_fds(stdin_fd, stdout_fd, stderr_fd);
+	char *argv[task->n_words];
+	fill_argv(task->word_list, argv);
+	if (execvp(argv[0], argv) < 0) {
+		fprintf(stderr, "Could not find program: %s\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void start_task(TASK *task, char *envp[]) {
+	pid_t pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "Could not spawn process\n");
+		return;
+	} else if (pid == 0) {
+		child_process_start_job(task, envp);
+	}
+	waitpid(pid, NULL, 0);
+}
