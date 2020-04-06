d5990b84a4e293b585528e2f84e34ac1c8bf04bc
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-06 02:39:00 -0400

    Updated src/main.c to use the job module

--- src/main.c
+++ src/main.c
@@ -4,8 +4,21 @@
 
 #include "input.h"
 #include "task.h"
+#include "job.h"
 
-int main(int argc, const char *argv[], const char *env[]) {
+void print_task_info(TASK *task) {
+	WORD_LIST *list = task->word_list;
+	while (list) {
+		printf("%s ", list->word);
+		list = list->next;
+	}
+	printf("\n");
+	printf("stdin: %s\n", task->stdin_path);
+	printf("stdout: %s\n", task->stdout_path);
+	printf("stderr: %s\n", task->stderr_path);
+}
+
+int main(int argc, const char *argv[], char *env[]) {
 	size_t n = getpagesize();
 	char *buf = malloc(n);
 	int result = 0;
@@ -17,17 +30,7 @@ int main(int argc, const char *argv[], const char *env[]) {
 		TASK *task = parse_task(buf);
 		if (task == TASK_EMPTY) continue;
 		else if (task == TASK_FAILED) break;
-		printf("Successful: %p\n", task);
-		printf("Input: %s\n", buf);
-		WORD_LIST *list = task->word_list;
-		while (list) {
-			printf("%s ", list->word);
-			list = list->next;
-		}
-		printf("\n");
-		printf("stdin: %s\n", task->stdin_path);
-		printf("stdout: %s\n", task->stdout_path);
-		printf("stderr: %s\n", task->stderr_path);
+		start_task(task, env);
 		free_task(task);
 	}
 	free(buf);
