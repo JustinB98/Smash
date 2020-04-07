09a314288eb39aadb400c80f5bca577129f2d1c4
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-07 02:17:57 -0400

    Updated src/main.c to remove memory issues

--- src/main.c
+++ src/main.c
@@ -25,23 +25,25 @@ int main(int argc, const char *argv[], char *env[]) {
 	size_t n = getpagesize();
 	char *buf = malloc(n);
 	int result = 0;
-	exit_code = 0;
 	signal_handlers_init();
+	TASK *task = NULL;
 	while (1) {
 		printf("smash> ");
 		fflush(stdout);
 		result = get_input(stdin, &buf, &n, child_reaper);
 		if (result < 0) break;
-		TASK *task = parse_task(buf);
+		task = parse_task(buf);
 		if (task == TASK_EMPTY) continue;
 		else if (task == TASK_FAILED) break;
 		int exit_smash = should_exit(task);
 		if (exit_smash > 0) break;
-		else if (exit_smash < 0) continue;
-		start_task(task, env);
+		else if (exit_smash == 0) {
+			start_task(task, env);
+		}
 		free_task(task);
 	}
 	free(buf);
+	if (task != NULL && task != TASK_FAILED) free_task(task);
 	if (result < 0) puts("");
 	return result < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
