0a70a6a00dd5980af6372c0893decbab778b651b
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-07 02:16:49 -0400

    Added exit codes in smash command module Also changed home path to be from the environment variable. Not sure if this is the correct, portable way to do this

--- src/smash_commands.c
+++ src/smash_commands.c
@@ -7,10 +7,12 @@
 #include <pwd.h>
 
 #include "task.h"
+#include "exit_code.h"
 
 static int smash_exit(TASK *task) {
 	if (task->n_words != 1) { 
 		fprintf(stderr, "smash: Too many arguments for exit\nUSAGE: exit\n");
+		set_exit_code_failure();
 		return -1;
 	} else return 1;
 }
@@ -18,33 +20,40 @@ static int smash_exit(TASK *task) {
 static int smash_cd(TASK *task) {
 	if (task->n_words > 2) {
 		fprintf(stderr, "smash: Too many arguments for cd\nUSAGE: cd [PATH]\n");
+		set_exit_code_failure();
 		return -1;
 	}
 	char *path = NULL;
 	if (task->n_words == 2) {
 		path = task->word_list->next->word;
 	} else if (task->n_words == 1) {
-		path = getpwuid(getuid())->pw_dir;
+		path = getenv("HOME");
 	}
 	int result = chdir(path);
 	if (result < 0) {
 		fprintf(stderr, "Could not find %s\n", path);
+		set_exit_code_failure();
 		return -1;
 	}
+	set_exit_code_success();
 	return 1;
 }
 
 static int smash_pwd(TASK *task) {
 	if (task->n_words > 1) {
 		fprintf(stderr, "smash: Too many arguments for pwd\nUSAGE: pwd\n");
+		set_exit_code_failure();
+		return -1;
 	}
 	char wd[PATH_MAX];
 	char *result = getcwd(wd, PATH_MAX);
 	if (result == NULL) {
 		fprintf(stderr, "smash: Could not get current working directory\n");
+		set_exit_code_failure();
 		return -1;
 	}
 	puts(wd);
+	set_exit_code_success();
 	return 1;
 }
 
@@ -57,5 +66,7 @@ int execute_smash_command(TASK *task) {
 
 int should_exit(TASK *task) {
 	char *cmd = task->word_list->word;
-	return !strcmp(cmd, "exit") && smash_exit(task);
+	if (!strcmp(cmd, "exit")) {
+		return smash_exit(task);
+	} else return 0;
 }
