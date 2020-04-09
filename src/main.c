56fdd1de290ea24c147e47f8d165ec5224304ee0
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-08 23:22:22 -0400

    Updated src/main.c to account for the job table * Initialize and finalize job table * Having start_task task struct (except if command is exit)

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include "signal_handlers.h"
 #include "smash_commands.h"
 #include "args.h"
+#include "job_table.h"
 
 void print_task_info(TASK *task) {
 	WORD_LIST *list = task->word_list;
@@ -35,6 +36,7 @@ static FILE *open_file_input() {
 
 int main(int argc, char *argv[], char *env[]) {
 	process_args(argc, argv);
+	job_table_init();
 	size_t n = getpagesize();
 	char *buf = malloc(n);
 	int result = 0;
@@ -53,15 +55,17 @@ int main(int argc, char *argv[], char *env[]) {
 		if (task == TASK_EMPTY) continue;
 		else if (task == TASK_FAILED) break;
 		int exit_smash = should_exit(task);
-		if (exit_smash > 0) break;
+		if (exit_smash > 0) {
+			free_task(task);
+			break;
+		}
 		else if (exit_smash == 0) {
 			start_task(task, env);
 		}
-		free_task(task);
 	}
+	job_table_fini();
 	if (file_input != stdin) fclose(file_input);
 	free(buf);
-	if (task != NULL && task != TASK_FAILED && task != TASK_EMPTY) free_task(task);
 	if (result < 0 && file_input == stdin) puts("");
 	return EXIT_SUCCESS;
 }
