3cbe60806b93e177f0425e56b0d98867c75e272d
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-08 23:05:57 -0400

    Fixed queue add and remove Root was never getting modified

--- src/queue.c
+++ src/queue.c
@@ -11,6 +11,7 @@ QUEUE *queue_init() {
 	if (queue == NULL) {
 		perror("Could not create a queue");
 	}
+	queue->root = NULL;
 	return queue;
 }
 
@@ -24,11 +25,23 @@ static QUEUE_NODE *create_queue_node(int data) {
 	return queue_node;
 }
 
+static QUEUE_NODE *queue_node_insert(QUEUE_NODE *node, QUEUE_NODE *prev, QUEUE_NODE *new_node) {
+	if (node == NULL) {
+		if (prev != NULL) prev->next = new_node;
+		return new_node;
+	} else if (new_node->data > node->data) {
+		new_node->next = node;
+		if (prev != NULL) prev->next = new_node;
+		return new_node;
+	} else {
+		return queue_node_insert(node->next, node, new_node);
+	}
+}
+
 void queue_insert(QUEUE *queue, int data) {
 	QUEUE_NODE *queue_node = create_queue_node(data);
 	if (queue_node == NULL) return;
-	queue_node->next = queue->root;
-	queue->root = queue_node;
+	queue->root = queue_node_insert(queue->root, NULL, queue_node);
 }
 
 int queue_peek(QUEUE *queue) {
@@ -48,17 +61,19 @@ int queue_pop(QUEUE *queue) {
 	return data;
 }
 
-void queue_remove(QUEUE *queue, int data) {
-	QUEUE_NODE *current = queue->root;
-	QUEUE_NODE *prev = NULL;
-	while (current) {
-		if (current->data == data) {
-			if (prev != NULL) prev->next = current->next;
-			free(current);
-		}
-		prev = current;
-		current = current->next;
+static void queue_node_remove(QUEUE_NODE *node, QUEUE_NODE *prev, int data) {
+	if (node == NULL){
+		return;
+	} else if (node->data == data) {
+		if (prev != NULL) prev->next = node->next;
+		free(node);
+	} else {
+		queue_node_remove(node->next, node, data);
 	}
+}
+
+void queue_remove(QUEUE *queue, int data) {
+	queue_node_remove(queue->root, NULL, data);
 }
 
 void queue_fini(QUEUE *queue) {
