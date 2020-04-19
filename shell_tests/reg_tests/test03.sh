b0b1b38f13fa0145b5fdc0bc5559c719e2c09213
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-19 18:04:19 -0400

    Added general command tests

--- shell_tests/reg_tests/test03.sh
+++ shell_tests/reg_tests/test03.sh
@@ -8,8 +8,22 @@ assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
 
 echo "============ TEST03.SH START ==============="
 
+test_name="ls Test 1"
+./smash <(echo ls) > test_file
+cmp test_file <(ls)
+$assert_success
 
+test_name="ls Test 2"
+./smash <(echo ls /tmp -al) > test_file
+cmp test_file <(ls /tmp -al)
+$assert_success
+
+test_name="wc Test"
+./smash <(echo wc ./smash) > test_file
+cmp test_file <(wc ./smash)
+$assert_success
 
 printf "==================== TEST03.SH END ====================\n"
 test_type="test03.sh"
+rm test_file
 . shell_tests/finish.sh
