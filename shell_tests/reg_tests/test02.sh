81e63e7339bf1fb9efbfbded7e7e3ba9ca6438a9
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-19 16:56:14 -0400

    Added echo tests

--- shell_tests/reg_tests/test02.sh
+++ shell_tests/reg_tests/test02.sh
@@ -8,7 +8,26 @@ assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
 
 echo "============ TEST02.SH START ==============="
 
+test_name="Non GNU Echo Test"
+./smash <(echo "echo --help") > test_file 2> /dev/null
+cmp test_file <(echo "--help")
+$assert_success
 
+test_name="Echo Test One Word"
+./smash <(echo "echo hello") > test_file 2> /dev/null
+cmp test_file <(echo "hello")
+$assert_success
+
+test_name="Echo Test Multiple Words"
+./smash <(echo "echo very long echo test") > test_file 2> /dev/null
+cmp test_file <(echo "very long echo test")
+$assert_success
+
+python -c "print('echo test sentence' * 100)" > static_file
+test_name="Echo Test Stress Test"
+./smash <(printf "echo " ; cat static_file) > test_file 2> /dev/null
+cmp test_file static_file
+$assert_success
 
 printf "==================== TEST02.SH END ====================\n"
 test_type="test02.sh"
