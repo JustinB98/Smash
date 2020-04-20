8f11a2c1b0f1ebc52fa97e4e4583c97f22655459
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-20 19:19:55 -0400

    Added globbing tests

--- shell_tests/ec_tests/test06.sh
+++ shell_tests/ec_tests/test06.sh
@@ -8,6 +8,17 @@ assert_fail=". shell_tests/assert_unsuccessful_exit.sh"
 
 echo "============ TEST06.SH START ==============="
 
+test_name="Glob Test General Tests"
+./smash <(printf "cd src\necho *.c | wc -l") > stdout_file
+cd src ; echo *.c | wc -l > ../actual_stdout_file ; cd ..
+cmp actual_stdout_file stdout_file
+$assert_success
+
+test_name="Glob Test No Files Found"
+./smash <(printf "echo *.badfileextension") > stdout_file
+echo *.badfileextension > actual_stdout_file
+cmp actual_stdout_file stdout_file
+$assert_success
 
 
 printf "==================== TEST06.SH END ====================\n"
