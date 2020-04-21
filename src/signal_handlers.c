d65cb2e86b3e73819f4cc357c806cfd18982f3c6
Justin Behrman <justin.behrman@stonybrook.edu>
2020-04-21 15:39:27 -0400

    Ignoring SIGTTIN and SIGTTOU signals Also added some code for getrusage that is #if 0 'ed out

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -5,11 +5,15 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+#include <sys/time.h>
+#include <sys/resource.h>
+
 #include "task.h"
 #include "pipeline.h"
 #include "job.h"
 #include "signal_handlers.h"
 #include "job_table.h"
+#include "debug.h"
 
 static void set_up_sigaction(struct sigaction *action, void (*handler)(int)) {
 	action->sa_handler = handler;
@@ -43,6 +47,8 @@ static void install_signal_handlers() {
 	install_signal_handler(SIGINT, sigint_handler);
 	install_signal_handler(SIGTSTP, sigstop_handler);
 	install_signal_handler(SIGCHLD, sigchld_handler);
+	install_signal_handler(SIGTTOU, SIG_IGN);
+	install_signal_handler(SIGTTIN, SIG_IGN);
 }
 
 void signal_handlers_init() {
@@ -50,13 +56,39 @@ void signal_handlers_init() {
 	/* TODO anything else? */
 }
 
+#if 0
+static void print_usage() {
+	struct rusage self_usage, child_usage;
+	int result = getrusage(RUSAGE_SELF, &self_usage);
+	if (result < 0) {
+		perror("self getrusage");
+		return;
+	}
+	result = getrusage(RUSAGE_CHILDREN, &child_usage);
+	if (result < 0) {
+		perror("child getrusage");
+		return;
+	}
+	printf("REAL=%lu.%lu USER=%lu.%lu SYSTEM=%lu.%lu\n",
+			self_usage.ru_utime.tv_sec,
+			self_usage.ru_utime.tv_usec,
+			child_usage.ru_utime.tv_sec,
+			child_usage.ru_utime.tv_usec,
+			child_usage.ru_stime.tv_sec,
+			child_usage.ru_stime.tv_usec);
+}
+#endif
+
 void reap_children() {
 	pid_t rpid;
 	int wstatus;
 	while ((rpid = waitpid(-1, &wstatus, WNOHANG | WUNTRACED | WCONTINUED)) > 0) {
+		print_debug_message("Reaped child with pid: %d", rpid);
 		if (WIFSIGNALED(wstatus)) {
+			// print_usage();
 			job_table_mark_as_aborted(rpid, 128 + WTERMSIG(wstatus));
 		} else if (WIFEXITED(wstatus) || WIFSIGNALED(wstatus)) {
+			// print_usage();
 			job_table_mark_as_done(rpid, WEXITSTATUS(wstatus));
 		} else if (WIFCONTINUED(wstatus)) {
 			job_table_change_status(rpid, RUNNING);
